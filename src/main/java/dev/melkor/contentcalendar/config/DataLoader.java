d616fbd276e6c48196b4beb671875422fedc27f9
Mel <meletioskorres@gmail.com>
2023-05-04 23:34:51 +0300

    Final version of project committing and trying to push to railway.app

--- src/main/java/dev/melkor/contentcalendar/config/DataLoader.java
+++ src/main/java/dev/melkor/contentcalendar/config/DataLoader.java
@@ -3,9 +3,9 @@ package dev.melkor.contentcalendar.config;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.melkor.contentcalendar.model.Content;
-import dev.melkor.contentcalendar.repository.ContentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import dev.melkor.contentcalendar.repository.ContentRepository;
 
 import java.io.InputStream;
 import java.util.List;
@@ -23,12 +23,14 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        if(repository.count() == 0) {
+        if (repository.count() == 0) {
             try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/content.json")) {
-                repository.saveAll(objectMapper.readValue(inputStream,new TypeReference<List<Content>>(){}));
+                repository.saveAll(objectMapper.readValue(inputStream, new TypeReference<List<Content>>() {
+                }));
             }
         }
     }
 
 }
+
 
